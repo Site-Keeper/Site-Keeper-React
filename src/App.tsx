a17a3816f9fa4d53288af8f6c2ec18a0aefaa2a1
betancito <jeronimobeta90@gmail.com>
2024-09-20 19:31:01 -0500

    Finished admin Cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import { RoutesWithNotFound } from './components/utilities/routes-with-not-found
 import { PublicRoutes } from './models/routes/routes.model';
 import { RiwiTheme } from './state/context/riwiTheme';
 import ClientLayout from './components/layout/client.layout';
+import DashboardCards from './components/pages/private/admin/components/AdminCards';
 
 function App() {
   return (
     <div className='app'>
-        <RiwiTheme>
-        <RoutesWithNotFound>
-          <Route element={<ClientLayout />}>
-            <Route path={PublicRoutes.HOME} element={<Home />} />
-          </Route>
-          <Route>
-            
-          </Route>
-        </RoutesWithNotFound>
-      </RiwiTheme>
+      <DashboardCards></DashboardCards>
     </div>
   );
 }
