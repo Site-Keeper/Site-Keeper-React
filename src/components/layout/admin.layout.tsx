0b2e6f56b95964d344e32345638e22f0e4cd05c4
18zuluaga <18zuluaga1989@gmail.com>
2024-09-24 15:17:24 -0500

    refactor lint and build

--- src/components/layout/admin.layout.tsx
+++ src/components/layout/admin.layout.tsx
@@ -2,8 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import { SideBarAdmin } from "./components/sideBarAdmin.component";
 import { Box } from "@mui/material";
-import { IUser } from "../../models/interfaces";
-import { emptyUserState } from "../../state/redux/states/user";
 
 export default function AdminLayout() {
   return (
